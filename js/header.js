c879e93913129dbc94604ec868551d67e48b856e
Sarfraz <alisarfraz900@gmail.com>
2022-04-14 15:39:05 +0530

    signIn-signUp-Functionality-added

--- js/header.js
+++ js/header.js
@@ -1,13 +1,14 @@
+var modal2 = document.getElementById("myModal2");
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("signup");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
@@ -19,7 +20,40 @@ span.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target == modal || event.target== modal2) {
     modal.style.display = "none";
+    
+    modal2.style.display = "none";
   }
 }
+
+// Get the modal
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("signin");
+var btn3 = document.getElementById('link')
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+btn3.onclick = function() {
+  modal2.style.display="none"
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal2) {
+//     modal2.style.display = "none";
+//   }
+// }
+
+// var link=getElementById('link')
